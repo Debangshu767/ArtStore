65993c8340d731132da3dbb0e153b90d4a9e91c1
Debangshu <debangshu182002@gmail.com>
2023-08-13 20:01:25 +0530

    fix

--- src/components/SliderItem.jsx
+++ src/components/SliderItem.jsx
@@ -8,7 +8,7 @@ const SliderItem = ({ title, image }) => {
         alt={title}
         className='object-cover w-full h-[500px] md:h-[600px] rounded-lg'
       />
-      <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-80'></div>
+      <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-100'></div>
       <div className='absolute inset-0 flex items-center justify-center text-red-100 text-4xl font-thin uppercase '>
         {title}
       </div>
