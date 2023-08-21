44cb5c091b383b3b7b1ab547c0baada9801708d1
Debangshu <debangshu182002@gmail.com>
2023-08-21 17:17:57 +0530

    logo

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,6 +3,7 @@ import { MdDesignServices } from 'react-icons/md';
 import { DiIllustrator } from 'react-icons/di';
 import { SiCanva } from 'react-icons/si';
 import { BiSolidLandscape } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 
 const Services = () => {
@@ -14,22 +15,22 @@ const Services = () => {
 
     <div className='flex flex-wrap m-2 gap-6  justify-center'>
 
-        <button className=' w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
+        <Link className=' w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
         <h1 className='text-9xl text-red-400 '><MdDesignServices/></h1>
         <h2 className='text-sm text-slate-500 capitalize font-bold p-2' >Digital Illustriations</h2>
-        </button>
-        <button className='w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
+        </Link>
+        <Link className='w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
         <h1 className='text-9xl text-red-400 '><DiIllustrator/></h1>
         <h2 className='text-sm text-slate-500 capitalize font-bold p-2' >Vector Designs</h2>
-        </button>
-        <button className='w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
+        </Link>
+        <Link className='w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
         <h1 className='text-9xl text-red-400 '><SiCanva/></h1>
         <h2 className='text-sm text-slate-500 capitalize font-bold p-2' >Canva Designs</h2>
-        </button>
-        <button className='w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
+        </Link>
+        <Link className='w-[45%] md:w-[20%] flex flex-col gap-3 items-center bg-slate-50 p-4 rounded-lg shadow-lg shadow-slate-300 hover:shadow-slate-400'>
         <h1 className='text-9xl text-red-400 '><BiSolidLandscape/></h1>
         <h2 className='text-sm text-slate-500 capitalize font-bold p-2' >Background Art</h2>
-        </button>
+        </Link>
 
 
 
